c7386a3db39cdf296ef80d0c42c70b1f46f7ac72
Anshul Gour <161227582+Anshulgour2002@users.noreply.github.com>
2026-03-31 10:28:45 +0530

    add this file

---
+++ practiceforiner/scripet.js
@@ -0,0 +1,42 @@
+// javascript is a programming language use to make web page intrective they can run inside the browser and helps in action like buttton click and animation etc
+// use to create interactive and dynamic web pages 
+// 1scripting - use DOM Manupulation 
+// 2programming - perform a task and instruction 
+// programmming language
+//1 syncronous 
+//2 asyncrononus
+// -------------------------------Syncronus------------------------------------------
+/*
+data types in js 
+1 primitive 
+string,boolean,number,null,undefined,
+2 non primitive - they can store a complex data
+an object , an array , a date 
+
+
+-----key work - predefined regerved word
+scoping - variable ki life kaha tak acces kar sakte hai 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+function test() {
+   const message = "Hello Anshul";
+   console.log(message); 
+}
+
+test();
+// console.log(message); // Error
